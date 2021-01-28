d136c30682f56deb9070776ba07a4ce8e5ff45c5
Youngseo Kim <zerowest1001@gmail.com>
2021-01-28 12:22:41 +0900

    [#1] feat: 프로젝트 생성

---
+++ src/test/java/dabang/star/cafe/CafeApplicationTests.java
@@ -0,0 +1,13 @@
+package dabang.star.cafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
