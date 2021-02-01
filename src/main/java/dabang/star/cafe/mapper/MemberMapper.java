395eff22c47c80201dda56be33a8247014e2d4f8
yjs <wotlr789@naver.com>
2021-02-01 12:53:25 +0900

    feat: 이메일 중복 검사 로직 추가
    회원가입시 이메일 중복 방지를 위한 검사 로직을 추가, 데이터베이스에 이미 존재하는 이메일이 있는지 확인하는 로직

--- src/main/java/dabang/star/cafe/mapper/MemberMapper.java
+++ src/main/java/dabang/star/cafe/mapper/MemberMapper.java
@@ -3,8 +3,12 @@ package dabang.star.cafe.mapper;
 import dabang.star.cafe.domain.Member;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.Optional;
+
 @Mapper
 public interface MemberMapper {
 
     void save(Member member);
+
+    Optional<String> findByEmail(String email);
 }
