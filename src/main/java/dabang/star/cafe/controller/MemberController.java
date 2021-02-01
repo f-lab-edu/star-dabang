02f8a40cb42244315b003f80dc4be9273e0bd318
yjs <wotlr789@naver.com>
2021-02-01 10:27:29 +0900

    fix: MemberForm -> MemberRequestDto로 클래스 이름 변경 및 검증값 추가
    명확한 역할 구분을 위해서 클래스 이름을 MemberForm에서 MemberRequestDto로 변경
    핸드폰 번호 자리수 제한

--- src/main/java/dabang/star/cafe/controller/MemberController.java
+++ src/main/java/dabang/star/cafe/controller/MemberController.java
@@ -1,12 +1,11 @@
 package dabang.star.cafe.controller;
 
 import dabang.star.cafe.domain.Member;
-import dabang.star.cafe.dto.MemberForm;
+import dabang.star.cafe.dto.MemberRequestDto;
 import dabang.star.cafe.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,9 +18,9 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping
-    public ResponseEntity join(@Valid @RequestBody MemberForm memberForm) {
+    public ResponseEntity join(@Valid @RequestBody MemberRequestDto memberRequestDto) {
 
-        memberService.join(new Member(memberForm));
+        memberService.join(new Member(memberRequestDto));
 
         return ResponseEntity.status(HttpStatus.OK).body("success");
     }
