132607759baf6afc6679016704a70f7ce46aa474
Youngseo Kim <zerowest1001@gmail.com>
2021-02-05 01:39:52 +0900

    [#3] refactor: 불필요한 코드 제거

--- src/main/java/dabang/star/cafe/MyBatisConfig.java
+++
@@ -1,8 +0,0 @@
-package dabang.star.cafe;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@Configuration
-public class MyBatisConfig {
-}
