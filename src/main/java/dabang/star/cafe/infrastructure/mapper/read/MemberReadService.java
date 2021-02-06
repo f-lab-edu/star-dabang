fc9f2c69a827092a6c30a8429a9a93e835d9c3b0
yjs <wotlr789@naver.com>
2021-02-06 15:09:43 +0900

    [#5] feat: MemberReadService에 findByEmail 추가
    유저에 대한 정보를 찾기 위해서 email 정보로 Member찾는 쿼리를 매핑

--- src/main/java/dabang/star/cafe/infrastructure/mapper/read/MemberReadService.java
+++ src/main/java/dabang/star/cafe/infrastructure/mapper/read/MemberReadService.java
@@ -1,9 +1,14 @@
 package dabang.star.cafe.infrastructure.mapper.read;
 
+import dabang.star.cafe.domain.member.Member;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.Optional;
 
 @Mapper
 public interface MemberReadService {
 
     boolean exist(String email);
+
+    Optional<Member> findByEmail(String email);
 }
