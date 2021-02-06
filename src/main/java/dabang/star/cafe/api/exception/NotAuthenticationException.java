cd98e9da35944d8a60071faf51d2c735435e0a0d
yjs <wotlr789@naver.com>
2021-02-06 15:11:49 +0900

    [#5] feat: 로그인 인증 실패 예외 클래스 추가
    로그인 실패시 에러처리를 위해서 NotAuthenticationException클래스를 추가

---
+++ src/main/java/dabang/star/cafe/api/exception/NotAuthenticationException.java
@@ -0,0 +1,8 @@
+package dabang.star.cafe.api.exception;
+
+public class NotAuthenticationException extends RuntimeException {
+
+    public NotAuthenticationException(String message) {
+        super(message);
+    }
+}
