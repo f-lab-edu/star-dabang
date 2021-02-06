5f13f6da57f51f08609d6abcb619d6a2c14cffc7
yjs <wotlr789@naver.com>
2021-02-06 15:12:46 +0900

    [#5] feat: MemberApi 로그인 추가
    로그인 요청을 받고 성공과 실패를 리턴한다

--- src/main/java/dabang/star/cafe/api/MemberApi.java
+++ src/main/java/dabang/star/cafe/api/MemberApi.java
@@ -1,5 +1,8 @@
 package dabang.star.cafe.api;
 
+import dabang.star.cafe.api.request.LoginRequest;
+import dabang.star.cafe.api.response.IdResponse;
+import dabang.star.cafe.domain.member.LoginService;
 import dabang.star.cafe.domain.member.Member;
 import dabang.star.cafe.api.request.SignUpRequest;
 import dabang.star.cafe.application.MemberApplicationService;
@@ -16,22 +19,32 @@ import javax.validation.Valid;
 public class MemberApi {
 
     private final MemberApplicationService memberApplicationService;
+    private final LoginService loginService;
 
     /**
      * 멤버 회원가입
      *
-     * @param  signUpRequest 회원가입 요청 폼
-     * @return 멤버 회원가입 완료시 HttpStatus.Ok, ID 반환 / 유효성 검증 에러시 HttpStatus.BAD_REQUEST, error message 반환
+     * @param  signUpRequest (email, password, nickname, telephone, birth)
+     * @return 멤버 회원가입 완료시 HttpStatus.Ok, ID 반환 / 유효성 검증 에러시 HttpStatus.BAD_REQUEST, error 반환
      */
     @PostMapping()
-    public ResponseEntity<Long> signUpMember(@Valid @RequestBody SignUpRequest signUpRequest) {
+    public ResponseEntity<IdResponse> signUpMember(@Valid @RequestBody SignUpRequest signUpRequest) {
 
-        Long id = memberApplicationService.join(new Member(signUpRequest));
+        Long saveId = memberApplicationService.join(new Member(signUpRequest));
 
-        return ResponseEntity.status(HttpStatus.CREATED).body(id);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new IdResponse(saveId));
     }
 
     /**
      * 로그인
+     * @param loginRequest (email, password)
+     * @return 로그인 완료시 HttpsStatus.Ok, ID 반환 / 로그인 실패시 HttpsStatus.UNAUTHORIZED, error 반환
      */
+    @PostMapping("/login")
+    public ResponseEntity loginMember(@Valid @RequestBody LoginRequest loginRequest) {
+
+        Long loginId = loginService.login(loginRequest);
+
+        return ResponseEntity.status(HttpStatus.OK).body(new IdResponse(loginId));
+    }
 }
