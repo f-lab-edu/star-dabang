868c686bc15c5d580f3a45620e0a32f29ed68a3b
yjs <wotlr789@naver.com>
2021-02-14 14:21:54 +0900

    [#5] refactor: 상수 리팩토링
    클래스명과 변수명 사이에 중복되는 이름을 삭제

--- src/main/java/dabang/star/cafe/utils/SessionKey.java
+++ src/main/java/dabang/star/cafe/utils/SessionKey.java
@@ -5,5 +5,5 @@ public class SessionKey {
     /**
      * 로그인 회원의 ID를 가져오는 세션 키
      */
-    public static final String SESSION_KEY_LOGIN_MEMBER_ID = "MEMBER";
+    public static final String LOGIN_MEMBER_ID = "MEMBER";
 }
