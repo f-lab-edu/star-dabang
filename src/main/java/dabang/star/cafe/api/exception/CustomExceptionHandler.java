fbaaed5de4b863534b0ba8a98df73e730f04cf05
yjs <wotlr789@naver.com>
2021-02-21 01:09:38 +0900

    [#5] fix: 예외 응답처리 수정
    명확성을 위해 json 형식으로 메시지와 응답 코드를 반환

--- src/main/java/dabang/star/cafe/api/exception/CustomExceptionHandler.java
+++ src/main/java/dabang/star/cafe/api/exception/CustomExceptionHandler.java
@@ -1,5 +1,6 @@
 package dabang.star.cafe.api.exception;
 
+import dabang.star.cafe.api.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,7 @@ public class CustomExceptionHandler {
      * 유효하지 않은 요청 값에 대하여 에러가 발생한다
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
 
         String target = e.getBindingResult()
                 .getTarget()
@@ -24,27 +25,42 @@ public class CustomExceptionHandler {
                 .getFieldError()
                 .getDefaultMessage();
 
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.BAD_REQUEST.value())
+                .message(errorMessage)
+                .build();
+
         log.warn("Not Valid Exception : {}", target, e);
 
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorMessage);
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 
     /**
      * 유니크한 값에 대하여 중복된 값으로 요청시에 에러가 발생한다
      */
     @ExceptionHandler(DuplicatedException.class)
-    public ResponseEntity handleDuplicatedException(DuplicatedException e) {
+    public ResponseEntity<ErrorResponse> handleDuplicatedException(DuplicatedException e) {
+
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.CONFLICT.value())
+                .message(e.getMessage())
+                .build();
 
         log.warn("The requested value is duplicated", e);
 
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(NotAuthenticationException.class)
-    public ResponseEntity handleNotAuthenticationException(NotAuthenticationException e) {
+    public ResponseEntity<ErrorResponse> handleNotAuthenticationException(NotAuthenticationException e) {
+
+        ErrorResponse response = ErrorResponse.builder()
+                .status(HttpStatus.UNAUTHORIZED.value())
+                .message(e.getMessage())
+                .build();
 
         log.warn("User failed authentication", e);
 
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+        return new ResponseEntity<>(response, HttpStatus.UNAUTHORIZED);
     }
 }
