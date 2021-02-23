040f1e3d05df0a59e58d8abe650d18928e69e649
Youngseo Kim <zerowest1001@gmail.com>
2021-02-23 21:17:38 +0900

    [#5] reafactor:리뷰 반영

--- src/main/java/dabang/star/cafe/infrastructure/redis/RedisConfig.java
+++
@@ -1,9 +0,0 @@
-package dabang.star.cafe.infrastructure.redis;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-@Configuration
-@EnableRedisHttpSession
-public class RedisConfig {
-}
