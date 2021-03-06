7a384f1f23cd81d0a3c09eef6e232b7235bc1250
zw <zerowest1001@gmail.com>
2021-03-06 13:45:28 +0900

    [#10] feat: 회원 정보 수정
    리뷰 반영하여 AOP 로직 변경

--- src/main/java/dabang/star/cafe/api/aop/AuthCheckAspect.java
+++ src/main/java/dabang/star/cafe/api/aop/AuthCheckAspect.java
@@ -5,9 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.http.HttpSession;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 
 import static dabang.star.cafe.utils.SessionKey.CURRENT_MEMBER_ID;
 import static dabang.star.cafe.utils.SessionKey.LOGIN_MEMBER_ID;
@@ -19,14 +22,14 @@ public class AuthCheckAspect {
 
     private final HttpSession httpSession;
 
-    @Around("@annotation(dabang.star.cafe.api.aop.MemberLoginCheck) && execution(* *(.., @SessionMemberId(Long)))")
+    @Around("@annotation(dabang.star.cafe.api.aop.MemberLoginCheck) && execution(* *(.., @SessionMemberId (Long), ..))")
     public Object memberLoginCheck(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
         Object memberId = httpSession.getAttribute(LOGIN_MEMBER_ID);
         return proceedWithMemberId(memberId, proceedingJoinPoint);
     }
 
-    @Around("@annotation(dabang.star.cafe.api.aop.CurrentMemberCheck) && execution(* *(.., @SessionMemberId(Long)))")
+    @Around("@annotation(dabang.star.cafe.api.aop.CurrentMemberCheck) && execution(* *(.., @SessionMemberId (Long), ..))")
     public Object currentMemberCheck(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
 
         Object memberId = httpSession.getAttribute(CURRENT_MEMBER_ID);
@@ -39,8 +42,15 @@ public class AuthCheckAspect {
         }
 
         Object[] args = proceedingJoinPoint.getArgs();
-        if (args[args.length - 1] == null) {
-            args[args.length - 1] = memberId;
+        Method method = ((MethodSignature) proceedingJoinPoint.getSignature()).getMethod();
+
+        Annotation[][] parameterAnnotations = method.getParameterAnnotations();
+        for (int idx = 0; idx < parameterAnnotations.length; idx++) {
+            for (Annotation a : parameterAnnotations[idx]) {
+                if (a.annotationType().equals(dabang.star.cafe.api.aop.SessionMemberId.class)) {
+                    args[idx] = memberId;
+                }
+            }
         }
 
         return proceedingJoinPoint.proceed(args);
