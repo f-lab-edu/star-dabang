71b3c0aaa3d1f1e027a2cede04325eef7557fed5
yjs <wotlr789@naver.com>
2021-03-08 22:36:58 +0900

    [#13] feat: schema.sql 변경
    관리자의 상품 관리의 용이성과 새로운 상품 유형과 카테고리를 추가하는 확장성을 위해서 테이블을 나누어 참조

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,10 +1,9 @@
-DROP TABLE IF EXISTS food_my_menu;
-DROP TABLE IF EXISTS drink_my_menu;
-DROP TABLE IF EXISTS drink_option;
+DROP TABLE IF EXISTS my_menu;
+DROP TABLE IF EXISTS product_option;
 DROP TABLE IF EXISTS additional_option;
-DROP TABLE IF EXISTS drink;
-DROP TABLE IF EXISTS food;
-DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS product;
+DROP TABLE IF EXISTS product_category;
+DROP TABLE IF EXISTS product_type;
 DROP TABLE IF EXISTS manager;
 DROP TABLE IF EXISTS office;
 DROP TABLE IF EXISTS members;
@@ -44,88 +43,70 @@ CREATE TABLE manager
     FOREIGN KEY (office_id) REFERENCES office (office_id)
 );
 
-CREATE TABLE category
+CREATE TABLE product_type
 (
-    category_id   BIGINT      NOT NULL AUTO_INCREMENT,
-    category_name VARCHAR(10) NOT NULL,
+    product_type_id   INT         NOT NULL AUTO_INCREMENT,
+    product_type_name VARCHAR(20) NOT NULL UNIQUE,
 
-    PRIMARY KEY (category_id)
+    PRIMARY KEY (product_type_id)
 );
 
-CREATE TABLE drink
+CREATE TABLE product_category
 (
-    drink_id    BIGINT       NOT NULL AUTO_INCREMENT,
-    category_id BIGINT       NOT NULL,
-    option_id   BIGINT       NOT NULL,
-    drink_name  VARCHAR(50)  NOT NULL,
-    price       INT          NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    image       VARCHAR(255) NOT NULL,
-    active      VARCHAR(1)   NOT NULL,
-    created_at  DATE,
+    category_id     INT         NOT NULL AUTO_INCREMENT,
+    category_name   VARCHAR(10) NOT NULL,
+    product_type_id INT         NOT NULL,
 
-    PRIMARY KEY (drink_id),
-    FOREIGN KEY (category_id) REFERENCES category (category_id)
+    PRIMARY KEY (category_id),
+    FOREIGN KEY (product_type_id) REFERENCES product_type (product_type_id)
+);
+
+CREATE TABLE product
+(
+    product_id   BIGINT       NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(20)  NOT NULL,
+    category_id  INT          NOT NULL,
+    price        INT          NOT NULL,
+    description  VARCHAR(255) NOT NULL,
+    image        VARCHAR(255) NOT NULL,
+    is_active    VARCHAR(1)   NOT NULL,
+    created_at   DATE,
+
+    PRIMARY KEY (product_id),
+    FOREIGN KEY (category_id) REFERENCES product_category (category_id)
 );
 
 CREATE TABLE additional_option
 (
-    additional_option_id    BIGINT NOT NULL AUTO_INCREMENT,
-    additional_option_name  BIGINT NOT NULL,
-    additional_option_limit BIGINT NOT NULL,
-    price                   INT    NOT NULL,
+    additional_option_id   BIGINT NOT NULL AUTO_INCREMENT,
+    additional_option_name BIGINT NOT NULL,
+    max_value              INT    NOT NULL,
+    price                  INT    NOT NULL,
 
     PRIMARY KEY (additional_option_id)
 );
 
-CREATE TABLE drink_option
+CREATE TABLE product_option
 (
-    drink_option_id      BIGINT     NOT NULL AUTO_INCREMENT,
+    product_option_id    BIGINT     NOT NULL AUTO_INCREMENT,
     additional_option_id BIGINT     NOT NULL,
-    drink_id             BIGINT     NOT NULL,
+    product_id           BIGINT     NOT NULL,
     option_value         VARCHAR(1) NOT NULL DEFAULT '0',
 
-    PRIMARY KEY (drink_option_id),
+    PRIMARY KEY (product_option_id),
     FOREIGN KEY (additional_option_id) REFERENCES additional_option (additional_option_id),
-    FOREIGN KEY (drink_id) REFERENCES drink (drink_id)
+    FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
 
-CREATE TABLE food
-(
-    food_id     BIGINT       NOT NULL AUTO_INCREMENT,
-    food_name   VARCHAR(50)  NOT NULL,
-    category_id BIGINT       NOT NULL,
-    description VARCHAR(255) NOT NULL,
-    price       INT          NOT NULL,
-    heat        VARCHAR(1)   NOT NULL,
-    image       VARCHAR(255) NOT NULL,
-    created_at  DATE         NOT NULL,
-
-    PRIMARY KEY (food_id),
-    FOREIGN KEY (category_id) REFERENCES category (category_id)
-);
-
-CREATE TABLE drink_my_menu
+CREATE TABLE my_menu
 (
     my_menu_id   BIGINT      NOT NULL AUTO_INCREMENT,
     my_menu_name VARCHAR(10) NOT NULL,
-    drink_id     BIGINT      NOT NULL,
+    product_id   BIGINT      NOT NULL,
     member_id    BIGINT      NOT NULL,
     option_info  JSON        NOT NULL,
 
     PRIMARY KEY (my_menu_id),
     FOREIGN KEY (member_id) REFERENCES members (member_id),
-    FOREIGN KEY (drink_id) REFERENCES drink (drink_id)
-);
-
-CREATE TABLE food_my_menu
-(
-    my_menu_id   BIGINT      NOT NULL AUTO_INCREMENT,
-    my_menu_name VARCHAR(10) NOT NULL,
-    food_id      BIGINT      NOT NULL,
-    member_id    BIGINT      NOT NULL,
-
-    PRIMARY KEY (my_menu_id),
-    FOREIGN KEY (member_id) REFERENCES members (member_id),
-    FOREIGN KEY (food_id) REFERENCES food (food_id)
+    FOREIGN KEY (product_id) REFERENCES product (product_id)
 );
