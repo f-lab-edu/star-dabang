d5cd698a1d9cfaff272068f4e6668d4ff64abce2
yjs <wotlr789@naver.com>
2021-03-31 20:18:40 +0900

    [#26] fix : Pagination 디폴트 값 상수 관리

--- src/main/java/dabang/star/cafe/utils/page/Pagination.java
+++ src/main/java/dabang/star/cafe/utils/page/Pagination.java
@@ -5,17 +5,21 @@ import lombok.Getter;
 @Getter
 public class Pagination {
 
+    private static final int DEFAULT_PAGE = 100;
+
+    private static final int DEFAULT_SIZE = 20;
+
     private Integer page;
 
     private Integer size;
 
     public Pagination(Integer page, Integer size) {
-        this.page = page == null ? 0 : page;
-        this.size = size == null ? 20 : size;
+        this.page = page == null ? DEFAULT_PAGE : page;
+        this.size = size == null ? DEFAULT_SIZE : size;
     }
 
     public int getOffset() {
         return page * size;
     }
-    
+
 }
