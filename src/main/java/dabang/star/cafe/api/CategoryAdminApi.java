738b2484bafa47e35bf45316eb18b3966c5bbe09
yjs <wotlr789@naver.com>
2021-04-05 23:29:02 +0900

    [#41] style : CategoryAdminApi 주석 수정

--- src/main/java/dabang/star/cafe/api/CategoryAdminApi.java
+++ src/main/java/dabang/star/cafe/api/CategoryAdminApi.java
@@ -7,6 +7,7 @@ import dabang.star.cafe.application.data.CategoryData;
 import dabang.star.cafe.application.data.EnumData;
 import dabang.star.cafe.domain.manager.Role;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -20,7 +21,7 @@ public class CategoryAdminApi {
     private final CategoryAdminService categoryAdminService;
 
     /**
-     * Admin이 새로운 카테고리를 등록하며 타입을 지정하기 위해서 조회한다
+     * Admin이 새로운 카테고리를 등록하기전에 타입을 지정하기 위해서 조회
      *
      * @return 조회 완료시 HttpStatus.Ok (List<EnumData>) 반환
      */
@@ -29,10 +30,16 @@ public class CategoryAdminApi {
     public List<EnumData> getTypes() {
 
         return categoryAdminService.getCategoryTypes();
-
     }
 
+    /**
+     * Admin이 새로운 카테고리를 등록
+     *
+     * @param categoryCreateCommand (name, categoryType)
+     * @return 등록 완료시 HttpStatus.CREATED (CategoryData) 반환
+     */
     @LoginCheck(role = Role.ADMIN)
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public CategoryData createCategory(@Valid @RequestBody CategoryCreateCommand categoryCreateCommand) {
 
