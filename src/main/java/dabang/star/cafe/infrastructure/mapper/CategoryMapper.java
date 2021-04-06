19354512fb0fb08d56cd3f6c6411138f0b9c4b7a
yjs <wotlr789@naver.com>
2021-04-06 21:35:04 +0900

    [#41] fix : CategoryMapper 사용하지 않는 메서드 삭제

--- src/main/java/dabang/star/cafe/infrastructure/mapper/CategoryMapper.java
+++ src/main/java/dabang/star/cafe/infrastructure/mapper/CategoryMapper.java
@@ -1,10 +1,7 @@
 package dabang.star.cafe.infrastructure.mapper;
 
-import dabang.star.cafe.application.data.CategoryData;
 import dabang.star.cafe.domain.category.Category;
 import org.apache.ibatis.annotations.Mapper;
-
-import java.util.Optional;
 
 @Mapper
 public interface CategoryMapper {
@@ -12,8 +9,6 @@ public interface CategoryMapper {
     void insert(Category category);
 
     void update(Category category);
-
-    Optional<CategoryData> getByName(String name);
 
     boolean exists(String name);
 
