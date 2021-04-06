04ebc9f911b74220d68154efa523cdf2475c6ad6
zw <zerowest1001@gmail.com>
2021-04-06 09:51:27 +0900

    [#40] chore: 추가 프로퍼티 관련 설정
    aws.yaml 파일에 있던 설정 중
    cloud.aws.s3.bucket을 application.yaml에 추가
    
    기존에 스프링 실행시 Map 으로 주던 방식에서
    spring.config.import=optioanl:aws.yaml추가하는 방식으로 변경

--- src/main/java/dabang/star/cafe/CafeApplication.java
+++ src/main/java/dabang/star/cafe/CafeApplication.java
@@ -1,22 +1,15 @@
 package dabang.star.cafe;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-import java.util.Map;
 
 @EnableAspectJAutoProxy
 @SpringBootApplication
 public class CafeApplication {
 
-    private static final String SPRING_CONFIG_LOCATION = "spring.config.location";
-    private static final String YAMLS = "classpath:/application.yaml,classpath:/aws.yaml";
-
     public static void main(String[] args) {
-        new SpringApplicationBuilder(CafeApplication.class)
-                .properties(Map.of(SPRING_CONFIG_LOCATION, YAMLS))
-                .run(args);
+        SpringApplication.run(CafeApplication.class, args);
     }
 
 }
