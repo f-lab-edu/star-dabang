4a4679a2088368165ee7f5f13e48dbb5119ceea9
yjs <wotlr789@naver.com>
2021-04-09 00:41:12 +0900

    [#41] fix : enum 관리 수정
    Json 관련 어노테이션을 활용하여 간소화

--- src/main/java/dabang/star/cafe/application/CategoryAdminService.java
+++ src/main/java/dabang/star/cafe/application/CategoryAdminService.java
@@ -2,28 +2,23 @@ package dabang.star.cafe.application;
 
 import dabang.star.cafe.application.command.CategoryCreateCommand;
 import dabang.star.cafe.application.data.CategoryData;
-import dabang.star.cafe.application.data.EnumData;
 import dabang.star.cafe.application.exception.DuplicatedException;
 import dabang.star.cafe.domain.category.Category;
 import dabang.star.cafe.domain.category.CategoryRepository;
+import dabang.star.cafe.domain.category.CategoryType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-
-import static dabang.star.cafe.application.EnumMapperService.CATEGORY_TYPE;
 
 @RequiredArgsConstructor
 @Service
 public class CategoryAdminService {
 
-    private final EnumMapperService enumMapperService;
     private final CategoryRepository categoryRepository;
 
-    public List<EnumData> getCategoryTypes() {
+    public CategoryType[] getCategoryTypes() {
 
-        return enumMapperService.get(CATEGORY_TYPE);
+        return CategoryType.values();
     }
 
     public CategoryData createCategory(CategoryCreateCommand categoryCreateCommand) {
