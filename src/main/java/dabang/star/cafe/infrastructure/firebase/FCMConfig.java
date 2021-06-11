b6a0355bb7a26b96bbe872c90cb0c8788a23a266
Youngseo Kim <zerowest1001@gmail.com>
2021-06-12 01:51:05 +0900

    [#85] chore: 파이어베이스 prod 프로파일 설정

--- src/main/java/dabang/star/cafe/infrastructure/firebase/FCMConfig.java
+++ src/main/java/dabang/star/cafe/infrastructure/firebase/FCMConfig.java
@@ -6,34 +6,70 @@ import com.google.firebase.FirebaseOptions;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
 @Log4j2
 @Configuration
 public class FCMConfig {
 
-    @Value("${firebase.config}")
-    private String FIREBASE_CONFIG_PATH;
-    private FirebaseApp firebaseApp;
+    @Configuration
+    @Profile("!prod")
+    static class NotProdFCMConfig {
 
-    @PostConstruct
-    public void init() {
-        try {
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream()))
-                    .build();
+        @Value("${firebase.config}")
+        private String FIREBASE_CONFIG_PATH;
 
-            if (FirebaseApp.getApps().isEmpty()) {
-                this.firebaseApp = FirebaseApp.initializeApp(options);
-                log.info("Firebase has been initialized");
-            } else {
-                this.firebaseApp = FirebaseApp.getInstance();
+        private FirebaseApp firebaseApp;
+
+        @PostConstruct
+        public void init() {
+            try {
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream()))
+                        .build();
+
+                if (FirebaseApp.getApps().isEmpty()) {
+                    this.firebaseApp = FirebaseApp.initializeApp(options);
+                    log.info("Firebase has been initialized");
+                } else {
+                    this.firebaseApp = FirebaseApp.getInstance();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        }
+    }
+
+    @Configuration
+    @Profile("prod")
+    static class ProdFCMConfig {
+
+        @Value("${firebase.config}")
+        private String FIREBASE_CONFIG;
+
+        private FirebaseApp firebaseApp;
+
+        @PostConstruct
+        public void init() {
+            try {
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(FIREBASE_CONFIG.getBytes())))
+                        .build();
+
+                if (FirebaseApp.getApps().isEmpty()) {
+                    this.firebaseApp = FirebaseApp.initializeApp(options);
+                    log.info("Firebase has been initialized");
+                } else {
+                    this.firebaseApp = FirebaseApp.getInstance();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 
